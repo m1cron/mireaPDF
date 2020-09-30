69f6b72c0c1af26e261eacbc668e3913bd41a25b
m1cron <m1cron4900@gmail.com>
2020-10-01 00:25:16 +0300

    burn test off

--- src/test/java/MyProxyTest.java
+++ src/test/java/MyProxyTest.java
@@ -7,7 +7,7 @@ public class MyProxyTest {
     public static void main(String[] args) {
         MyProxy myProxy = new MyProxy();
         myProxy.getNewProxy();
-        for(int i = 0; i < 100; i++) {
+        for(int i = 0; i < 1; i++) {
             System.out.printf("proxy ip   -->>  %s\tport -->>  %d\tproxy mode -->>  %s\t", myProxy.getIp(), myProxy.getPort(), myProxy.getProxyMode());
             System.out.println(UtilsForIO.readStringFromURL("https://api.github.com/repos/m1cron/java_rtu/contents/src/ru/micron/task2", myProxy));
             System.out.printf("proxy ip   -->>  %s\tport -->>  %d\tproxy mode -->>  %s\t", myProxy.getIp(), myProxy.getPort(), myProxy.getProxyMode());
