243f3b9309fa493c14fe351427b63b2aa05e2dc6
m1cron <m1cron4900@gmail.com>
2020-10-14 14:06:55 +0300

    added test for make PDF+DOCX

---
+++ src/test/java/MakeDocsTest.java
@@ -0,0 +1,10 @@
+import ru.micron.MakeDocuments;
+
+public class MakeDocsTest {
+    public static void main(String[] args) {
+        MakeDocuments docs = new MakeDocuments();
+        docs.makePdf("index.html", "pdf.pdf");
+        docs.makeWord("pdf.pdf", "word.pdf");
+        docs.closeDriver();
+    }
+}
