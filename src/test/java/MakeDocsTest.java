b7ed8c3c3ac7a00488aa577fcfd291620b9bc0d9
m1cron <m1cron4900@gmail.com>
2020-10-14 14:29:37 +0300

    fixed test

--- src/test/java/MakeDocsTest.java
+++ src/test/java/MakeDocsTest.java
@@ -4,7 +4,7 @@ public class MakeDocsTest {
     public static void main(String[] args) {
         MakeDocuments docs = new MakeDocuments();
         docs.makePdf("index.html", "pdf.pdf");
-        docs.makeWord("pdf.pdf", "word.pdf");
+        docs.makeWord("pdf.pdf", "word.docx");
         docs.closeDriver();
     }
 }
