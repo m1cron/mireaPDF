a6a0f1b7551a5d22a605c28e6a0e372fbc2388b4
m1cron <m1cron4900@gmail.com>
2020-10-14 14:03:43 +0300

    fixed tests for PDF

--- src/test/java/MakePdfTest.java
+++ src/test/java/MakePdfTest.java
@@ -1,29 +1,10 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.io.File;
-import java.util.concurrent.TimeUnit;
-
+import ru.micron.MakePdf;
 
 public class MakePdfTest {
 
     public static void main(String[] argv) {
-        String fileInputPath = "./templates/index.ftl";
-        File file = new File(fileInputPath);
-
-        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-
-        WebDriver driver = new ChromeDriver();
-
-
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        driver.get("https://deftpdf.com/ru/html-to-pdf");
-
-        driver.findElement(By.xpath("//input[@type='file']")).sendKeys(file.getAbsolutePath());
-        driver.findElement(By.xpath("//a[@id='HTMLFileToPDF']")).click();
-
-        driver.findElement(By.className("download_file_name")).click();
+        MakePdf make = new MakePdf();
+        make.makePdf("index.html", "pdf.pdf");
+        make.closeDriver();
     }
 }
