9aa6c345cb551f51b454a96598490c091c84db10
note-pc <m1cron4900@gmail.com>
2020-10-20 21:57:32 +0300

    fixed json pars from file

--- src/main/java/ru/micron/json/GetJsonFromFile.java
+++ src/main/java/ru/micron/json/GetJsonFromFile.java
@@ -1,32 +1,21 @@
 package ru.micron.json;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import ru.micron.utils.UtilsForIO;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.Arrays;
 import java.util.Map;
 
-class Practice {
-    public PracticeFields[] practice;
+class ReportJson {
+    public String target, theory, step_by_step,conclusion, literature;
 
     @Override
     public String toString() {
-        return "Practice{" +
-                "practice=" + Arrays.toString(practice) +
-                '}';
-    }
-}
-
-class PracticeFields {
-    public String target, theory, conclusion, literature;
-
-    @Override
-    public String toString() {
-        return "JsonFile{" +
+        return "ReportJson{" +
                 "target='" + target + '\'' +
                 ", theory='" + theory + '\'' +
+                ", step_by_step='" + step_by_step + '\'' +
                 ", conclusion='" + conclusion + '\'' +
                 ", literature='" + literature + '\'' +
                 '}';
@@ -39,12 +28,14 @@ public class GetJsonFromFile extends UtilsForIO {
     public static void parseJson(Map<String, String> map, int pracNum) {
         try {
             Gson gson = new Gson();
-            Practice groups = gson.fromJson(readFileFromRes(fileJson, Charset.defaultCharset()), Practice.class);
+            ReportJson[] arr = gson.fromJson(readFileFromRes(fileJson, Charset.defaultCharset()), ReportJson[].class);
+            ReportJson obj = arr[pracNum];
             map.put("prac_number", Integer.toString(pracNum));
-            map.put("target_content", groups.practice[pracNum].target);
-            map.put("teor_content", groups.practice[pracNum].theory);
-            map.put("conclusion_content", groups.practice[pracNum].conclusion);
-            map.put("literature_content", groups.practice[pracNum].literature);
+            map.put("target_content", obj.target);
+            map.put("teor_content", obj.theory);
+            map.put("step_by_step", obj.step_by_step);
+            map.put("conclusion_content", obj.conclusion);
+            map.put("literature_content", obj.literature);
         } catch (IOException e) {
             e.printStackTrace();
         }
