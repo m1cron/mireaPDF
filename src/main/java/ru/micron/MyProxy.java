aad162292155f54866ed28ba803c2a88925da58d
m1cron <m1cron4900@gmail.com>
2020-10-24 13:28:15 +0300

    added synch for getNewProxy, added volatile for Proxy

--- src/main/java/ru/micron/MyProxy.java
+++ src/main/java/ru/micron/MyProxy.java
@@ -6,17 +6,15 @@ import com.google.gson.JsonObject;
 import ru.micron.interfaces.ReadStringFromURL;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class MyProxy implements ReadStringFromURL {
-    private final String proxyApi;
-    private Proxy proxy;
-    private String ip;
-    private int port;
     protected final Gson gson;
+    private final String proxyApi;
+    private volatile Proxy proxy;
 
     public MyProxy(String proxyPing) {
         this.proxyApi = "https://www.proxyscan.io/api/proxy?format=json&uptime=75&last_check=600&ping=" + proxyPing;
@@ -24,39 +22,33 @@ public class MyProxy implements ReadStringFromURL {
         getNewProxy();
     }
 
-    public void getNewProxy() {
+    public synchronized void getNewProxy() {
         JsonArray proxyList = gson.fromJson(readStringFromURL(proxyApi), JsonArray.class);
         JsonObject proxyObj = (JsonObject) proxyList.get(0);
 
-        ip = proxyObj.get("Ip").toString().replace("\"", " ").trim();
-        port = proxyObj.get("Port").getAsInt();
+        String ip = proxyObj.get("Ip").toString().replace("\"", " ").trim();
+        int port = proxyObj.get("Port").getAsInt();
         String proxyMode = proxyObj.getAsJsonArray("Type").get(0).toString();
 
         System.setProperty("socksProxyVersion", proxyMode.contains("4") ? "4" : "5");
         proxy = new Proxy(proxyMode.contains("SOCKS") ? Proxy.Type.SOCKS : Proxy.Type.HTTP, new InetSocketAddress(ip, port));
-        System.out.printf("connect to %s\t\t\t%d\t%s\n", ip, port, proxyMode);
+        System.out.println("getNewProxy >> connect to " + ip + "\t\t\t" + port + "\t\t" + proxyMode + "\t\tfrom  " + Thread.currentThread().getName());
     }
 
     public String readStringFromURL(String url) {
-        InputStream urlCon = null;
+        URLConnection urlCon;
         try {
-            urlCon = new URL(url).openConnection().getInputStream();
+            urlCon = new URL(url).openConnection();
+            urlCon.setConnectTimeout(2000);
+            urlCon.setReadTimeout(3000);
+            return scanInStream(urlCon.getInputStream());
         } catch (IOException e) {
             System.out.println("Enabled proxy!");
-            readStringFromURL(url, this);
+            return readStringFromURL(url, this);
         }
-        return scanInStream(urlCon);
     }
 
     public Proxy getProxy() {
         return proxy;
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public int getPort() {
-        return port;
     }
 }
--- src/main/java/ru/micron/interfaces/ReadStringFromURL.java
+++ src/main/java/ru/micron/interfaces/ReadStringFromURL.java
@@ -5,30 +5,23 @@ import ru.micron.MyProxy;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public interface ReadStringFromURL {
 
-    default String readStringFromURL(String url) {
-        InputStream urlCon = null;
-        try {
-            urlCon = new URL(url).openConnection().getInputStream();
-        } catch (IOException e) {
-            System.out.println("Turn on proxy!");
-        }
-        return scanInStream(urlCon);
-    }
-
     default String readStringFromURL(String url, MyProxy myProxy) {
-        InputStream urlCon;
+        URLConnection urlCon;
         try {
-            urlCon = new URL(url).openConnection(myProxy.getProxy()).getInputStream();
+            urlCon = new URL(url).openConnection(myProxy.getProxy());
+            urlCon.setConnectTimeout(2000);
+            urlCon.setReadTimeout(3000);
+            return scanInStream(urlCon.getInputStream());
         } catch (IOException e) {
             myProxy.getNewProxy();
             return readStringFromURL(url, myProxy);
         }
-        return scanInStream(urlCon);
     }
 
     default String scanInStream(InputStream stream) {
