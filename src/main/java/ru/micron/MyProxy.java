dfb5d6cac7e04c93abc2996a8c48795b5d4e1ea3
m1cron <m1cron4900@gmail.com>
2020-10-26 21:59:36 +0300

    deleted readString iunterface, rewritten scanInStream method

--- src/main/java/ru/micron/MyProxy.java
+++ src/main/java/ru/micron/MyProxy.java
@@ -3,15 +3,19 @@ package ru.micron;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import ru.micron.interfaces.ReadStringFromURL;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.util.Scanner;
 
-public class MyProxy implements ReadStringFromURL {
+public class MyProxy {
     protected final Gson gson;
     private final String proxyApi;
     private volatile Proxy proxy;
@@ -46,6 +50,40 @@ public class MyProxy implements ReadStringFromURL {
             System.out.println("Enabled proxy!");
             return readStringFromURL(url, this);
         }
+    }
+
+    public String readStringFromURL(String url, MyProxy myProxy) {
+        URLConnection urlCon;
+        try {
+            urlCon = new URL(url).openConnection(myProxy.getProxy());
+            urlCon.setConnectTimeout(2000);
+            urlCon.setReadTimeout(3000);
+            return scanInStream(urlCon.getInputStream());
+        } catch (IOException e) {
+            myProxy.getNewProxy();
+            return readStringFromURL(url, myProxy);
+        }
+    }
+
+    private String scanInStream(InputStream stream) {
+        BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        try {
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return response.toString();
     }
 
     public Proxy getProxy() {
