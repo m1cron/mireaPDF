5385b9048156dc47d014f630b0c2c8ffdc0cba69
m1cron <m1cron4900@gmail.com>
2020-10-28 01:34:18 +0300

    added tests for MakeDocs class

---
+++ src/test/java/ru/micron/MakeDocumentsTest.java
@@ -0,0 +1,34 @@
+package ru.micron;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MakeDocumentsTest {
+
+    @Test
+    public void makePdf() {
+        MakeDocuments docs = new MakeDocuments();
+        docs.makePdf("index.html", "pdf.pdf");
+        docs.closeDriver();
+    }
+
+    @Test
+    public void makeWord() {
+        MakeDocuments docs = new MakeDocuments();
+        docs.makePdf("index.html", "pdf.pdf");
+        docs.makeWord("pdf.pdf", "word.docx");
+        docs.closeDriver();
+    }
+
+    @Test
+    public void downloadFile() {
+        String[] urls = { "https://file-examples-com.github.io/uploads/2017/10/file-sample_150kB.pdf",
+                          "https://file-examples-com.github.io/uploads/2017/10/file-example_PDF_500_kB.pdf",
+                          "https://file-examples-com.github.io/uploads/2017/10/file-example_PDF_1MB.pdf" };
+        int i = 0;
+        for (String url : urls) {
+            MakeDocuments.downloadFile(url, "TMP/file_" + i++ + ".pdf");
+        }
+    }
+}
