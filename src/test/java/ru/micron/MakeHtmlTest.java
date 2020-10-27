f1d6a4673c65a385cca31d92d92e872d68acfa12
m1cron <m1cron4900@gmail.com>
2020-10-28 01:40:01 +0300

    added test for MakeHtml class

---
+++ src/test/java/ru/micron/MakeHtmlTest.java
@@ -0,0 +1,16 @@
+package ru.micron;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MakeHtmlTest {
+
+    @Test
+    public void makeHtml() {
+        String url = "https://github.com/m1cron/java_rtu/tree/master/src/ru/micron/task1";
+        new MakeHtml("./templates", "index.ftl").
+                makeHtml(new MakeMap(url).
+                            getMap(), "./index.html");
+    }
+}
