01e2d2e26560a43b1920bf7b36ef3e83bef14e4b
m1cron <m1cron4900@gmail.com>
2020-10-28 00:51:45 +0300

    added tests for GetDate class

--- src/test/java/ru/micron/GetDateTest.java
+++ src/test/java/ru/micron/GetDateTest.java
@@ -1,12 +1,40 @@
 package ru.micron;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GetDateTest {
 
     @Test
-    public void GetDateTest() {
+    public void fillMap() {
         GetDate date = new GetDate();
-        System.out.println(date.getDay() + "\t" + date.getMonth() + "\t" + date.getYear());
+
+        Map<String, String> map = new HashMap<>(3, 1);
+        date.fillMap(map);
+
+        Assert.assertNotNull(map.get("day"));
+        Assert.assertNotNull(map.get("month"));
+        Assert.assertNotNull(map.get("year"));
+    }
+
+    @Test
+    public void getYear() {
+        GetDate date = new GetDate();
+        Assert.assertNotNull(date.getYear());
+    }
+
+    @Test
+    public void getMonth() {
+        GetDate date = new GetDate();
+        Assert.assertNotNull(date.getMonth());
+    }
+
+    @Test
+    public void getDay() {
+        GetDate date = new GetDate();
+        Assert.assertNotNull(date.getDay());
     }
 }
