7feb44e3cb688b57d4bb2dd4698c5f64be6ef4af
m1cron <m1cron4900@gmail.com>
2020-10-28 01:12:02 +0300

    added tests for Github class

--- src/test/java/ru/micron/GithubTest.java
+++ src/test/java/ru/micron/GithubTest.java
@@ -3,14 +3,25 @@ package ru.micron;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class GithubTest {
 
     @Test
-    public void GithubWork() {
-        String url = "https://github.com/m1cron/java_rtu/blob/master/src/ru/micron/task4/DrawIO/Human.drawio";
-        Assert.assertNotEquals(new Github(url, false, "100").getCodeArr(), null);
+    public void Github() {
+        String url = "https://github.com/m1cron/java_rtu/blob/master/src/ru/micron/task4";
+        new Github(url, false, "100").getCodeArr().forEach(Assert::assertNotNull);
     }
 
+    @Test
+    public void recursSearchGit() {
+        String url = "https://github.com/m1cron/java_rtu/blob/master/src/ru/micron/task2";
+        Github gh = new Github(url, false, "100");
+        gh.getCodeArr().forEach(Assert::assertNotNull);
+    }
+
+    @Test
+    public void parseUrl() {
+        String url = "https://github.com/m1cron/java_rtu/blob/master/src/ru/micron";
+        String destUrl = "https://api.github.com/repos/m1cron/java_rtu/contents/src/ru/micron";
+        Assert.assertEquals(Github.parseUrl(url), destUrl);
+    }
 }
